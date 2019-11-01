66fc097fa4d00957f75f3feab76756b034077deb
Alexandre <alex.morad12@hotmail.com>
2019-11-01 11:11:59 -0400

    fixed origin

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,14 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
 import userApi from './endpoints/user';
 import blogApi from './endpoints/blog';
 import postApi from './endpoints/post';
 
-console.log(process.env.NODE_ENV);
+let origin = 'http://localhost:3000';
 if (process.env.NODE_ENV !== 'production') {
   require('../config');
+  origin = 'https://blogr-webapp.herokuapp.com/';
 }
 
 const PORT = process.env.PORT || 8080;
@@ -20,8 +18,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', origin);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
@@ -34,10 +31,6 @@ app.use(function(req, res, next) {
   });
 });
 
-app.get('/temp', function(req, res){
-  return res.json({ response: 'success' })
-});
-
 app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`),
+  console.log(`Server is running on Port:${PORT}`),
 );
